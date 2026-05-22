9293ddd7ca50ddde342c2f15d0678e3fbb4b70e3
Rafael <101014805+rafaelsp-14@users.noreply.github.com>
2026-05-22 12:32:16 +0200

    Update main.py
    - latency
    - multithread

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 import os
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
 
 subprocess.call('cls' if os.name == "nt" else "clear", shell=True)
 
@@ -15,14 +16,21 @@ print ("_" * 60)
 
 t1 = datetime.now()
 
-try: 
-    for port in range(1,5000):
+def scan(port): 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.05)
+        sock.settimeout(0.2)
+        start = datetime.now()
         result = sock.connect_ex((remoteServerIP, port))
+        end = datetime.now()
+        latency = (end - start).total_seconds() * 1000
         if result == 0:
-            print ("Port {}: Open".format(port))
+            print (f"Port {port}: Open ({latency} ms)")
         sock.close()
+        
+try:
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        executor.map(scan, range(1, 5000)) 
+            
 except KeyboardInterrupt:
     print("You pressed Ctrl+C")
     sys.exit()
@@ -31,6 +39,6 @@ except socket.gaierror:
 except socket.error:
     print("Couldn't connect to server")
 
-t2 = datetime.now()
-total = t2 - t1
+t4 = datetime.now()
+total = t4 - t1
 print("Scanning Completed in : ", total)
